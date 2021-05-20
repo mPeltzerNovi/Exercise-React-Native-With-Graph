67d60d9efdf896f09c89f9cd159cd11b44906712
mPeltzerNovi <m.peltzer@novi-education.nl>
2021-05-20 14:48:39 +0200

    Test

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>He Martin!</Text>
+      <Text>Check!</Text>
       <StatusBar style="auto" />
     </View>
   );
