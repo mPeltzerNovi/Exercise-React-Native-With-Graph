86076321cbc3e38724ce785837a840fa0c6334b4
mPeltzerNovi <m.peltzer@novi-education.nl>
2021-05-21 12:51:02 +0200

    Test MET SafeAreaView

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default class FixedDimensionsBasics extends Component {
   render() {
     return (
-        <View>
+        <SafeAreaView>
           <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
           <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
           <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
-        </View>
+        </SafeAreaView>
     )
   }
 }
