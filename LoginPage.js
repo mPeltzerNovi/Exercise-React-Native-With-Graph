b101ea10a2375ed261214eef9a992bfbd503b357
mPeltzerNovi <m.peltzer@novi-education.nl>
2021-05-27 10:08:39 +0200

    Login screen and Home screen navigation

---
+++ LoginPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from "react-native";
+
+
+const LoginPage = () => {
+    return (
+        <View>
+            <Text>I am the Login page</Text>
+        </View>
+    )
+}
+
+export default LoginPage;
